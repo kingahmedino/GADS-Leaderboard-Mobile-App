e5a91d1cb175d2ea2625fa3b1eb208bfb23007ae
kingahmedino <ahmedino001>
2020-09-01 15:29:02 +0100

    Added models

---
+++ app/src/main/java/com/example/gadsleaderboardapp/models/LearningLeader.kt
@@ -0,0 +1,4 @@
+package com.example.gadsleaderboardapp.models
+
+class LearningLeader(var name: String, var hours: Int, var country: String, var badgeUrl: String) {
+}
---
+++ app/src/main/java/com/example/gadsleaderboardapp/models/SkillIQLeader.kt
@@ -0,0 +1,4 @@
+package com.example.gadsleaderboardapp.models
+
+class SkillIQLeader(var name: String, var score: Int, var country: String, var badgeUrl: String) {
+}
