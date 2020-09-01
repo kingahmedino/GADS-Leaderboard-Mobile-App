03187cbd9ae5fc5fc43058245f18f9dd31407f35
kingahmedino <ahmedino001>
2020-09-01 11:59:15 +0100

    Added Learning Leaders and Skill IQ fragments

---
+++ app/src/main/java/com/example/gadsleaderboardapp/fragments/SkillIQLeadersFragment.kt
@@ -0,0 +1,19 @@
+package com.example.gadsleaderboardapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gadsleaderboardapp.R
+
+class SkillIQLeadersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_skill_iq_leaders, container, false)
+    }
+
+}
