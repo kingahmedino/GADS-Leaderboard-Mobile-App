f53571b9f436f2c7af6fe949a0f3566cf9183961
kingahmedino <ahmedino001>
2020-09-02 18:22:14 +0100

    Added back button to SubmitActivity

--- app/src/main/java/com/example/gadsleaderboardapp/SubmitActivity.kt
+++ app/src/main/java/com/example/gadsleaderboardapp/SubmitActivity.kt
@@ -2,10 +2,21 @@ package com.example.gadsleaderboardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_submit.*
 
 class SubmitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_submit)
+        setSupportActionBar(toolbar)
+        toolbarWorks()
+    }
+
+    private fun toolbarWorks() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
