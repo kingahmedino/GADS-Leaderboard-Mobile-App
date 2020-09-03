d98a1c1dd700a455ca19845858a7f7aff0d84d36
kingahmedino <ahmedino001>
2020-09-03 06:48:04 +0100

    Added SubmitActivityViewModel and BackendSubmitInterface for project submission

---
+++ app/src/main/java/com/example/gadsleaderboardapp/backend/BackendSubmitInterface.kt
@@ -0,0 +1,29 @@
+package com.example.gadsleaderboardapp.backend
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface BackendSubmitInterface {
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    @FormUrlEncoded
+    fun submitProject(
+        @Field("entry.1824927963")email: String,
+        @Field("entry.1877115667")firstName: String,
+        @Field("entry.2006916086")lastName: String,
+        @Field("entry.284483984")projectLink: String
+    ): Call<Unit>
+
+    companion object{
+        operator fun invoke(): BackendSubmitInterface{
+            return Retrofit.Builder()
+                .baseUrl("https://docs.google.com/forms/d/e/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(BackendSubmitInterface::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gadsleaderboardapp/viewmodels/SubmitActivityViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.gadsleaderboardapp.viewmodels
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+
+class SubmitActivityViewModel: ViewModel() {
+    fun onSubmitButtonClick(view: View){
+
+    }
+}
